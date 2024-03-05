6674eeba01c64f2f844120da0cd7494fb6795b9e
lucas081297 <65791119+lucas081297@users.noreply.github.com>
2024-03-05 08:16:26 -0300

    Update Questao-5.js

--- Questao-5.js
+++ Questao-5.js
@@ -4,7 +4,7 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Entre com um texto qualquer ', (answer) => {
+rl.question('Entre com um texto qualquer: ', (answer) => {
     var invertedString = ''
     for(let i=answer.length-1;i>=0;i--){
         invertedString+=answer[i]
